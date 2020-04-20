b232eff833e2de36c69b3ff64314ca6ff3ef4bd4
Matthew Follegot <matthewfollegot@gmail.com>
2020-04-20 12:05:47 -0400

    demonstrating pointers with 2d arrays

--- 2dArray.c
+++ 2dArray.c
@@ -1,35 +1,84 @@
 #include <stdio.h>
 
-	int main() {
-		/* TODO: declare the 2D array grades here */
-        int grades[2][5];
-		float average;
-		int i;
-		int j;
+int day_of_year(int year, int month, int day);
+void month_day(int day, int yearday, int *pmonth, int *pday);
+int day_of_year_pointer(int year,int month, int day);
+void month_day_pointer(int year, int yearday, int *pmonth, int *pday);
 
-		grades[0][0] = 80;
-		grades[0][1] = 70;
-		grades[0][2] = 65;
-		grades[0][3] = 89;
-		grades[0][4] = 90;
 
-		grades[1][0] = 85;
-		grades[1][1] = 80;
-		grades[1][2] = 80;
-		grades[1][3] = 82;
-		grades[1][4] = 87;
+struct Date
+{
+    int year;
+    int month;
+    int day;
+};
 
-		/* TODO: complete the for loop with appropriate terminating conditions */
-		for (i = 0; i < sizeof(grades)/sizeof(grades[0]); i++) {
-			average = 0;
-			for (j = 0; j < sizeof(grades[0])/sizeof(grades[0][0]); j++) {
-				average += grades[i][j];
-			}
-            
-			/* TODO: compute the average marks for subject i */
-            average /= (sizeof(grades[0])/sizeof(grades[0][0]));
-			printf("The average marks obtained in subject %d is: %.2f\n", i, average);
-		}
+int main()
+{
+    struct Date date;
+    date.year = 1999;
+    date.day = 178;
+    
+    int *pmonth = &date.month;
+    int *pday = &date.day;
 
-		return 0;
-	}
+    month_day_pointer(date.year, date.day, pmonth, pday);
+
+    printf("month: %d \t day: %d\n", *pmonth, *pday);
+    printf("day of year: %d\n", day_of_year_pointer(date.year, date.month, date.day));
+}
+
+static char daytab[2][13] = {
+    {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+    {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
+
+/* day_of_year: set day of year from month & day */
+int day_of_year(int year, int month, int day)
+{
+    int i, leap;
+    leap = (year%4 == 0 && year%100 != 0) || year%400 == 0;
+    for (i = 1; i < month; i++)
+        day += daytab[leap][i];
+    return day;
+}
+
+/* month_day: set month, day from day of year */
+void month_day(int year, int yearday, int *pmonth, int *pday)
+{
+    int i, leap;
+    leap = (year%4 == 0 && year%100 != 0) || year%400 == 0;
+    for (i = 1; yearday > daytab[leap][i]; i++)
+        yearday -= daytab[leap][i];
+    *pmonth = i;
+    *pday = yearday;
+}
+
+/* pointer versions */
+
+int day_of_year_pointer(int year,int month, int day)
+{
+    int i, leap;
+    char *p;
+
+    leap = (year%4 == 0 && year%100 != 0) || year%400 == 0;
+    p = &daytab[leap][1];
+
+    for (i = 1; i < month; i++)
+        day += *p++;
+    return day;
+}
+
+void month_day_pointer(int year, int yearday, int *pmonth, int *pday)
+{
+    int i, leap;
+    char *p;
+
+    leap = (year%4 == 0 && year%100 != 0) || year%400 == 0;
+    p = &daytab[leap][1];
+    for (i = 1; yearday > *p; i++)
+        yearday -= *p++;
+    
+    *pmonth = i;
+    *pday = yearday;
+}
